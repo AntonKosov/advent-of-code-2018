a8ef5021a25b279a64ee5ee61848672106937031
Anton Kosov <anton@kosov.dev>
2024-12-28 11:19:52 -0600

    day 01

---
+++ day01/part2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/AntonKosov/advent-of-code-2018/aoc/input"
+	"github.com/AntonKosov/advent-of-code-2018/aoc/slice"
+	"github.com/AntonKosov/advent-of-code-2018/aoc/transform"
+)
+
+func main() {
+	answer := process(read())
+	fmt.Printf("Answer: %v\n", answer)
+}
+
+func read() []int {
+	lines := input.Lines()
+	lines = lines[:len(lines)-1]
+
+	return slice.Map(lines, func(line string) int { return transform.StrToInt(line) })
+}
+
+func process(numbers []int) int {
+	freq := 0
+	frequencies := map[int]bool{freq: true}
+	for {
+		for _, num := range numbers {
+			freq += num
+			if frequencies[freq] {
+				return freq
+			}
+			frequencies[freq] = true
+		}
+	}
+}
